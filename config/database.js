9b8ae9f3c4a5a234077cf68678abe2b1146d67b4
luuhoangphuu <hoangphu57789@gmail.com>
2026-09-08 15:55:03 +0000

    Explicitly load pg module in Sequelize

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg'); // Bắt buộc load pg để Sequelize nhận diện
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  dialectModule: pg, // Chỉ định rõ module pg
   dialectOptions: {
     ssl: {
       require: true,
