b3879152abef8a0c00b79378ebaf50061278dbeb
luuhoangphuu <hoangphu57789@gmail.com>
2026-09-08 16:13:38 +0000

    Remove dialectModule to use default pg driver

--- config/database.js
+++ config/database.js
@@ -1,11 +1,9 @@
 const { Sequelize } = require('sequelize');
-const { neon } = require('@neondatabase/serverless');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
-  dialectModule: neon, // Dùng driver Neon thay vì pg
   dialectOptions: {
     ssl: {
       require: true,
